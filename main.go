267bac9fce67626749aa38282432132c4fcf658a
Kaleb Freeman <kaleb.freeman@outlook.com>
2025-01-08 08:06:49 -0600

    clean up and comments

--- main.go
+++ main.go
@@ -1,40 +1,37 @@
 package main
 
-// Welcome to
 // __________         __    __  .__                               __
 // \______   \_____ _/  |__/  |_|  |   ____   ______ ____ _____  |  | __ ____
 //  |    |  _/\__  \\   __\   __\  | _/ __ \ /  ___//    \\__  \ |  |/ // __ \
 //  |    |   \ / __ \|  |  |  | |  |_\  ___/ \___ \|   |  \/ __ \|    <\  ___/
 //  |________/(______/__|  |__| |____/\_____>______>___|__(______/__|__\\_____>
 //
-// This file can be a nice home for your Battlesnake logic and helper functions.
-//
-// To get you started we've included code to prevent your Battlesnake from moving backwards.
-// For more info see docs.battlesnake.com
 
 import (
 	"log"
 	"math/rand"
 )
 
-// info is called when you create your Battlesnake on play.battlesnake.com
-// and controls your Battlesnake's appearance
-// TIP: If you open your Battlesnake URL in a browser you should see this data
+// Info is the server ping
 func info() BattlesnakeInfoResponse {
 	log.Println("INFO")
 
 	return BattlesnakeInfoResponse{
 		APIVersion: "1",
-		Author:     "kfree98", // TODO: Your Battlesnake username
-		Color:      "#FF0000", // TODO: Choose color
-		Head:       "pixel",   // TODO: Choose head
-		Tail:       "pixel",   // TODO: Choose tail
+		Author:     "kfree98",
+		Color:      "#FF0000",
+		Head:       "pixel",
+		Tail:       "pixel",
 	}
 }
 
 // start is called when your Battlesnake begins a game
 func start(state GameState) {
+	log.Println("##############################################################")
 	log.Println("GAME START")
+	log.Printf("Game ID: %s", state.Game.ID)
+	log.Println("Game Ruleset:", state.Game.Ruleset.Name)
+	log.Println("##############################################################")
 }
 
 // end is called when your Battlesnake finishes a game
@@ -42,29 +39,19 @@ func end(state GameState) {
 	log.Printf("GAME OVER\n\n")
 }
 
-// move is called on every turn and returns your next move
-// Valid moves are "up", "down", "left", or "right"
-// See https://docs.battlesnake.com/api/example-move for available data
 func move(state GameState) BattlesnakeMoveResponse {
 
-	// We've included code to prevent your Battlesnake from moving backwards
-	myHead := state.You.Body[0] // Coordinates of your head
-	myNeck := state.You.Body[1] // Coordinates of your "neck"
+	myHead := state.You.Body[0]
+	myNeck := state.You.Body[1]
 
-	// TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
 	boardWidth := state.Board.Width
 	boardHeight := state.Board.Height
-	// borderCoords := generateBorderCoords(boardWidth, boardHeight)
+
+	// Calculate the available moves
 	safeMoves := getAvailableMoves(myHead, myNeck, boardWidth, boardHeight, state, state.You.Body)
 	log.Println("Safe moves", len(safeMoves))
-	// TODO: Step 2 - Prevent your Battlesnake from colliding with itself
-	// mybody := state.You.Body
 
-	// TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
-	// opponents := state.Board.Snakes
-
-	// Are there any safe moves left?
-
+	// If no safe moves, lay down in defeat..
 	if len(safeMoves) == 0 {
 		log.Printf("MOVE %d: No safe moves detected! Moving down\n", state.Turn)
 		return BattlesnakeMoveResponse{Move: "down", Shout: "** PaNiK!! **"}
@@ -72,9 +59,6 @@ func move(state GameState) BattlesnakeMoveResponse {
 
 	// Choose a random move from the safe ones
 	nextMove := safeMoves[rand.Intn(len(safeMoves))]
-
-	// TODO: Step 4 - Move towards food instead of random, to regain health and survive longer
-	// food := state.Board.Food
 
 	log.Printf("MOVE %d: %s\n", state.Turn, nextMove.Direction)
 	return BattlesnakeMoveResponse{Move: nextMove.Direction}
@@ -128,6 +112,7 @@ func getAvailableMoves(head, neck Coord, boardWidth, boardHeight int, state Game
 
 	}
 
+	// Filter out unsafe moves
 	var safeMoves []Move
 	for _, move := range potentialMoves {
 		if move.Safe {
@@ -135,5 +120,6 @@ func getAvailableMoves(head, neck Coord, boardWidth, boardHeight int, state Game
 		}
 	}
 
+	// Return the safe moves
 	return safeMoves
 }
